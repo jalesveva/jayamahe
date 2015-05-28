3567d1ce3ceb6d5e75d0a003dd267d0515cf2eab
Dhi Aurrahman <diorahman@rockybars.com>
2015-05-28 17:59:58 +0700

    Add geojson layer as marker clusters

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var map = L.map('map').setView([-1.269160, 116.825264], 5);
 L.mapbox.accessToken = 'pk.eyJ1IjoiZGlvcmFobWFuIiwiYSI6IkNQLXNqX2MifQ.2U5fcgqoKq5qxwkbdyjRYQ';
-L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+var tiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
   id: 'diorahman.m9o84opn',
   maxZoom: 18,
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
     'Imagery © <a href="http://mapbox.com">Mapbox</a>'
-}).addTo(map);
+});
+
 var markerOptions = {
   radius: 6,
   fillColor: '#ff33cc',
@@ -18,14 +19,16 @@ var markerOptions = {
 var request = new XMLHttpRequest();
 request.open('GET', 'http://dev.aegis.co.id:9001/points', true);
 request.onload = function() {
-  L.geoJson(JSON.parse(this.responseText), {
-    pointToLayer: function(feature, latlng) {
-      return L.circleMarker(latlng, markerOptions);
-    },
+  var markers = L.markerClusterGroup();
+  var geoJson = L.geoJson(JSON.parse(this.responseText), {
     onEachFeature : function(feature, layer) {
       layer.bindPopup(feature.properties.NAMA_KAPAL_VMS);
     }
-  }).addTo(map);
+  });
+  markers.addLayer(geoJson);
+  map.addLayer(tiles);
+  map.addLayer(markers);
+  map.fitBounds(markers.getBounds());
 }
 request.send();
 
