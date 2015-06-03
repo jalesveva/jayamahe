d6baf2c321ca3203b9163175b5379ce4bc282605
Dhi Aurrahman <diorahman@rockybars.com>
2015-06-04 06:18:49 +0700

    Add new data with wpp field

--- tools/write.js
+++ tools/write.js
@@ -1,9 +1,12 @@
 var convert = require('../lib/convert');
+
+PREFIX = 'data-2015-06-03';
+
 var fs = require('fs');
-convert({ path : __dirname + '/../data/dummy/data-2015-05-22.xml'}, function(err, json){
+convert({ path : __dirname + '/../data/dummy/' + PREFIX + '.xml'}, function(err, json){
   if (err)
     throw err;
-  fs.writeFileSync(__dirname + '/../data/dummy/data-2015-05-22-min.json', JSON.stringify(json.features));
-  fs.writeFileSync(__dirname + '/../data/dummy/data-2015-05-22.geojson', JSON.stringify(json, null, 2));
-  fs.writeFileSync(__dirname + '/../data/dummy/data-2015-05-22-min.geojson', JSON.stringify(json));
+  fs.writeFileSync(__dirname + '/../data/dummy/' + PREFIX + '-min.json', JSON.stringify(json.features));
+  fs.writeFileSync(__dirname + '/../data/dummy/' + PREFIX + '.geojson', JSON.stringify(json, null, 2));
+  fs.writeFileSync(__dirname + '/../data/dummy/' + PREFIX + '-min.geojson', JSON.stringify(json));
 });
